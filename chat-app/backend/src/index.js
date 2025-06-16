7385a0e4d9c88db711ed86c6a9e34e37d7546243
Shahinur Rahman Shahin <srshahin111@gmail.com>
2025-06-17 00:47:11 +0600

    chat-app backend initialization

---
+++ chat-app/backend/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server is running on port: " + PORT);
+  connectDB();
+});
