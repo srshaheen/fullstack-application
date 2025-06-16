11db219770e732f695c1675c1eb79948c9d9caa2
Shahinur Rahman Shahin <srshahin111@gmail.com>
2025-06-17 01:57:54 +0600

    remove security issue

--- chat-app/backend/src/index.js
+++ chat-app/backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 
@@ -14,6 +15,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
